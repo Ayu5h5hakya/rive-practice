2410fbacb290ffa524e8bfe43fe7239a72dc31dc
Ayu5h5hakya <qwerty.ayush5@gmail.com>
2023-06-29 20:39:40 +0545

    add base scaffold

--- rive_sta/lib/main.dart
+++ rive_sta/lib/main.dart
@@ -13,7 +13,7 @@ const _primaryColor = Color(0xFF57A5E0);
 
 void main() => runApp(MaterialApp(
   title: 'Rive Example',
-  home: RiveExampleApp(),
+  home: RiveCalendarExample(),
   darkTheme: ThemeData.dark().copyWith(
     scaffoldBackgroundColor: _backgroundColor,
     appBarTheme: const AppBarTheme(backgroundColor: _appBarColor),
@@ -22,59 +22,28 @@ void main() => runApp(MaterialApp(
   themeMode: ThemeMode.dark,
 ));
 
-class RiveExampleApp extends StatefulWidget {
-  const RiveExampleApp({Key? key}) : super(key: key);
-
+class RiveCalendarExample extends StatefulWidget {
   @override
-  State<StatefulWidget> createState() => _RiveExampleAppState();
+  State<StatefulWidget> createState() => _RiveCalendarExampleState();
 }
 
-class _RiveExampleAppState extends State<RiveExampleApp> {
-  final _pages = [
-     _Page('Simple Asset Animation', BasicAnim()),
-     _Page('Simple Toggle Animation', PlayPauseAnimation()),
-     _Page('Simple One Shot Animation', PlayOneShotAnimation()),
-     _Page('Simple One Shot Animation', ExampleStateMachine()),
-  ];
+class _RiveCalendarExampleState extends State<RiveCalendarExample> {
 
+  final PageController _controller = PageController();
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) => Scaffold(
-    appBar: AppBar(title: const Text('Rive Examples'),),
-    body: Center(
-      child: ListView.separated(itemBuilder: (context, index) => _NavButton(page: _pages[3]), separatorBuilder: (context, index) => const SizedBox(height: 16), itemCount: 2,),
-    ),
-  );
-}
-
-class _Page {
-  final String name;
-  final Widget page;
-
-  const _Page(this.name, this.page);
-}
- 
-class _NavButton extends StatelessWidget {
-  const _NavButton({required this.page});
-
-  final _Page page;
-
-  @override
-  Widget build(BuildContext context) => Center(
-    child: ElevatedButton(
-      child: SizedBox(
-        width: 250,
-        child: Center(
-          child: Text(
-            page.name,
-            style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-          ),
-        ),
-      ),
-      onPressed: () {
-        Navigator.push(context, MaterialPageRoute<void>(
-          builder: (_) => page.page
-        ),);
-      },
-    ),
+    appBar: AppBar(title: const Text('Calendar'),),
+    body: PageView.builder(
+      controller: _controller,
+      scrollDirection: Axis.vertical,
+      itemBuilder: (_,index) {
+        return Text(index.toString());
+      }),
   );
 }
