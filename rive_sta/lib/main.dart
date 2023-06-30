f0779000099f0e14a6c3f39e026f6d2ee9072462
Ayu5h5hakya <qwerty.ayush5@gmail.com>
2023-06-30 18:11:02 +0545

    add prototype coffee fill animation

--- rive_sta/lib/main.dart
+++ rive_sta/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:rive/rive.dart';
 
 import 'example_state_machine.dart';
@@ -13,7 +14,7 @@ const _primaryColor = Color(0xFF57A5E0);
 
 void main() => runApp(MaterialApp(
   title: 'Rive Example',
-  home: RiveCalendarExample(),
+  home: LiquidDownload(),
   darkTheme: ThemeData.dark().copyWith(
     scaffoldBackgroundColor: _backgroundColor,
     appBarTheme: const AppBarTheme(backgroundColor: _appBarColor),
@@ -22,28 +23,75 @@ void main() => runApp(MaterialApp(
   themeMode: ThemeMode.dark,
 ));
 
-class RiveCalendarExample extends StatefulWidget {
+class LiquidDownload extends StatefulWidget {
+  const LiquidDownload({Key? key}) : super(key: key);
+
   @override
-  State<StatefulWidget> createState() => _RiveCalendarExampleState();
+  State<LiquidDownload> createState() => _LiquidDownloadState();
 }
 
-class _RiveCalendarExampleState extends State<RiveCalendarExample> {
+class _LiquidDownloadState extends State<LiquidDownload> {
+  /// Tracks if the animation is playing by whether controller is running.
+  bool get isPlaying => _controller?.isActive ?? false;
 
-  final PageController _controller = PageController();
+  Artboard? _riveArtboard;
+  StateMachineController? _controller;
+  SMIInput<double>? _progress;
 
   @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
+  void initState() {
+    super.initState();
+
+    // Load the animation file from the bundle, note that you could also
+    // download this. The RiveFile just expects a list of bytes.
+    rootBundle.load('assets/coffee.riv').then(
+      (data) async {
+        // Load the RiveFile from the binary data.
+        final file = RiveFile.import(data);
+
+        // The artboard is the root of the animation and gets drawn in the
+        // Rive widget.
+        final artboard = file.mainArtboard;
+        var controller =
+            StateMachineController.fromArtboard(artboard, 'State Machine 1');
+        if (controller != null) {
+          artboard.addController(controller);
+          _progress = controller.findInput('scrollpercent');
+        }
+        setState(() => _riveArtboard = artboard);
+      },
+    );
   }
+
   @override
-  Widget build(BuildContext context) => Scaffold(
-    appBar: AppBar(title: const Text('Calendar'),),
-    body: PageView.builder(
-      controller: _controller,
-      scrollDirection: Axis.vertical,
-      itemBuilder: (_,index) {
-        return Text(index.toString());
-      }),
-  );
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Liquid Download'),
+      ),
+      body: Center(
+        child: _riveArtboard == null
+            ? const SizedBox()
+            : GestureDetector(
+              onTap: () {
+                print('object');
+              },
+                onVerticalDragUpdate: (details) {
+                              if (details.delta.dy > 0) {
+              // Down Swipe
+              print(details.delta.dy);
+              _progress!.value += 1;
+            } else if (details.delta.dy < 0) {
+              // Up Swipe
+             _progress!.value -= 1;
+             print(details.delta.dy);
+            }
+                },
+                child :Rive(
+                          artboard: _riveArtboard!,
+                        ),
+              ),
+      ),
+    );
+  }
 }
